edff99cdaa83a52c1064dbc10f3731c0aaaff692
Lorena Araujo Almeida <87384833+lorenaaraujoo@users.noreply.github.com>
2023-04-09 15:24:11 -0300

    Update prevencao-incendio.cpp

--- prevencao-incendio/prevencao-incendio.cpp
+++ prevencao-incendio/prevencao-incendio.cpp
@@ -1,18 +1,20 @@
 
 void setup(){
+  Serial.begin(9600);
   pinMode(A0, INPUT);
   pinMode(11, OUTPUT);
   pinMode(4, OUTPUT);
 }
 
 void loop(){
-  if (true) {
+  Serial.println(analogRead(A0));
+  if (analogRead(A0)>600) {
     digitalWrite(11, HIGH);
     digitalWrite(4, HIGH);
-    delay(1000); // Wait for 1000 millisecond(s)
+    delay(1000); 
     digitalWrite(11, LOW);
     digitalWrite(4, LOW);
-    delay(1000); // Wait for 1000 millisecond(s)
+    delay(1000); 
   } else {
     digitalWrite(11, LOW);
     digitalWrite(4, LOW);
